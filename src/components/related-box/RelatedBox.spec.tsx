a7852514f70cc32e65310b02205ed816727b8e4e
willyamdev <willyamvinicius1900@gmail.com>
2022-06-24 18:06:32 -0300

    new test

---
+++ src/components/related-box/RelatedBox.spec.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import RelatedBox from "./RelatedBox";
+
+describe("StandardSlider", () => {
+  test("renders the StandardSlider component", () => {
+    render(<RelatedBox />);
+  });
+});
