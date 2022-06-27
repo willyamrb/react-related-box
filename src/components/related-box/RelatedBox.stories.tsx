62782473e247b41902c4c9ff7ba0a7deb77dcce0
willyamdev <willyamvinicius1900@gmail.com>
2022-06-27 10:35:29 -0300

    feat: added 2 different examples

--- src/components/related-box/RelatedBox.stories.tsx
+++ src/components/related-box/RelatedBox.stories.tsx
@@ -10,12 +10,12 @@ export default {
 } as ComponentMeta<typeof RelatedBox>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof RelatedBox> = (args) => {
+export const SimpleData: ComponentStory<typeof RelatedBox> = (args) => {
   const allData = ["will", "mathew", "john", "maria"];
 
   const [query, setQuery] = useState("");
 
-  const filteredData = allData.filter((d) => d.startsWith(query));
+  const filteredData = allData.filter((name) => name.startsWith(query));
 
   return (
     <RelatedBox {...args}>
@@ -32,6 +32,48 @@ const Template: ComponentStory<typeof RelatedBox> = (args) => {
   );
 };
 
-export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {};
+export const ArrayOfObjects: ComponentStory<typeof RelatedBox> = (args) => {
+  const allData = [
+    {
+      name: "will",
+      age: 22,
+    },
+    {
+      name: "mathew",
+      age: 44,
+    },
+    {
+      name: "john",
+      age: 16,
+    },
+    {
+      name: "maria",
+      age: 29,
+    },
+  ];
+
+  const [query, setQuery] = useState("");
+
+  const filteredData = allData.filter((person) =>
+    person.name.startsWith(query)
+  );
+
+  return (
+    <RelatedBox {...args}>
+      <RelatedBox.InputText
+        placeholder="Type something..."
+        onChange={(v) => setQuery(v)}
+      />
+      <RelatedBox.SuggestionsBox
+        data={filteredData}
+        objectKeyValue="name"
+        onSelectItem={(i) => console.log(i)}
+        renderItem={(v) => <div style={{ padding: "5px 0px" }}>{v.name}</div>}
+      />
+    </RelatedBox>
+  );
+};
+
+// export const Default = SimpleData.bind({});
+// // More on args: https://storybook.js.org/docs/react/writing-stories/args
+// Default.args = {};
